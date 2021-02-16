1c34239137070a6bd48440511a43248201419e01
sochoac1 <68884467+sochoac1@users.noreply.github.com>
2021-02-15 20:23:04 -0500

    Add files via upload

---
+++ talleres/taller04/DigraphAM.java
@@ -0,0 +1,40 @@
+  
+ 
+
+import java.util.ArrayList;
+
+/**
+ * Implementacion de un grafo dirigido usando matrices de adyacencia
+ *
+ * @author Mauricio Toro, Mateo Agudelo, <su nombre>
+ */
+public class DigraphAM extends Digraph {
+	
+	int[][] matrix;
+
+	public DigraphAM(int size) {
+		super(size);
+		
+		matrix = new int[size][size];
+	}
+
+	public void addArc(int source, int destination, int weight) {
+	
+		matrix[source][destination] = weight;
+	}
+
+	public ArrayList<Integer> getSuccessors(int vertex) {
+	
+		ArrayList<Integer> s = new ArrayList<>();
+		for (int i = 0; i < size; ++i)
+			if (matrix[vertex][i] != 0)
+				s.add(i);
+		return s.size() == 0 ? null : s;
+	}
+
+	public int getWeight(int source, int destination) {
+		
+		return matrix[source][destination];
+	}
+
+}
